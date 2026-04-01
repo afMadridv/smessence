c67bbd6c5bf236ab8b38a5b930ece547e506f12e
Andres Madrid <amxd100@gmail.com>
2026-04-01 14:12:25 -0500

    carrito funcional + paginas de productos + limpiar carrito

--- js/script.js
+++ js/script.js
@@ -1,27 +1,31 @@
-//CARRUSEL
-
-
+// CARRUSEL
 let index = 0;
 const images = document.querySelectorAll('.carrusel-imagenes img');
 
 function mostrarImagen() {
     images.forEach((img, i) => {
-        img.classList.remove('activo'); // Elimina la clase activa de todas las imágenes
+        img.classList.remove('activo');
         if (i === index) {
-            img.classList.add('activo'); // Agrega la clase activa a la imagen actual
+            img.classList.add('activo');
         }
     });
-    index = (index + 1) % images.length; // Incrementa el índice y reinicia si es necesario
+
+    index = (index + 1) % images.length;
 }
 
-setInterval(mostrarImagen, 5500); // Cambia la imagen cada 5.5 segundos
+if (images.length > 0) {
+    setInterval(mostrarImagen, 5500);
+}
 
+// GRID OCULTO
+const verMasButton = document.getElementById('ver-mas');
 
-//Grid oculto
-document.getElementById('ver-mas').addEventListener('click', function() {
-    const elementosOcultos = document.querySelectorAll('.oculto');
-    elementosOcultos.forEach(function(elemento) {
-        elemento.style.display = 'block'; // Muestra los elementos ocultos
+if (verMasButton) {
+    verMasButton.addEventListener('click', function () {
+        const elementosOcultos = document.querySelectorAll('.oculto');
+        elementosOcultos.forEach(function (elemento) {
+            elemento.style.display = 'block';
+        });
+        this.style.display = 'none';
     });
-    this.style.display = 'none'; // Oculta el botón después de hacer clic
-});
+}
